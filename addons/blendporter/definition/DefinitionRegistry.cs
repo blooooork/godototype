282e312ef677c5a28e4caf8b2c6493a0ae6dfdd2
LittleCarlito <littlecarlitogithub@gmail.com>
2026-03-22 08:50:21 -0500

    fix: Node3D definition

--- addons/blendporter/definition/DefinitionRegistry.cs
+++ addons/blendporter/definition/DefinitionRegistry.cs
@@ -10,7 +10,10 @@ public static class DefinitionRegistry
 {
     private static readonly System.Collections.Generic.Dictionary<Type, List<PropertyDefinition>> Registry = new()
     {
-        // TODO Need to make a Node3D definition
+        [typeof(Node3D)] = [
+            PropertyDefinitions.PositionDefinition,
+            PropertyDefinitions.TransformDefinition
+        ],
         [typeof(RigidBody3D)] = [
             PropertyDefinitions.GravityScaleDefinition, 
             PropertyDefinitions.MassDefinition,
